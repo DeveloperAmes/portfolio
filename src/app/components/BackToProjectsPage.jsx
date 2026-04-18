863e016a73404caa84e62672961f53734c32a244
AMY TUCK <amyltuck@hotmail.com>
2026-04-18 16:49:12 +0100

    chore: add arrow icon to backtoprojects component

--- src/app/components/BackToProjectsPage.jsx
+++ src/app/components/BackToProjectsPage.jsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function BackToProjectsPage() {
-  return <Link href={"/projects"}>Back to Projects</Link>;
+  return (
+    <Link href={"/projects"}>
+      <Image src={"/assets/backarrow.png"} alt="" height={20} width={20} />
+      Back to Projects
+    </Link>
+  );
 }
