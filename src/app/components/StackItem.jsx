c10de5d879b8a56524bbc9ded789dfa2f808653d
AMY TUCK <amyltuck@hotmail.com>
2026-04-19 15:52:15 +0100

    style: make stack images bigger and apply module styles

--- src/app/components/StackItem.jsx
+++ src/app/components/StackItem.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import stackItemStyles from "@/app/styles/stackitem.module.css"
 
 export default function StackItem({ src, alt, height, width }) {
-  return <Image src={src} alt={alt} height={50} width={50} />;
+  return <Image src={src} alt={alt} height={75} width={75} className={stackItemStyles.img}/>;
 }
