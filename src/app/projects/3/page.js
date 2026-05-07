bb0278c8c21d9becf81906e1d5e142a613c08724
AMY TUCK <amyltuck@hotmail.com>
2026-05-07 13:39:13 +0100

    chore: add content to project 3 page

--- src/app/projects/3/page.js
+++ src/app/projects/3/page.js
@@ -5,7 +5,28 @@ export default function Project3() {
   return (
     <main>
       <BackToProjectsPage />
-      <ProjectPage />
+      <ProjectPage
+        h1content="Kickabout Site"
+        p1content={
+          "A website that helps to connect users who want to meet up and play sport casually, making exercise less restrictive and more fun."
+        }
+        p2content={
+          "...the large proportion of people who don't have the time or energy to regularly attend local clubs or learn all the rules of a sport and just want to move their bodies for fun."
+        }
+        p3content={
+          <ul>
+            <li>{"React"}</li>
+            <li>{"NextJS"}</li>
+            <li>{"CSS Modules"}</li>
+            <li>{"PostgresSQL"}</li>
+            <li>{"Clerk auth"}</li>
+            <li>{"React Icons"}</li>
+            <li>{"UK Towns API"}</li>
+            <li>{"Flaticons"}</li>
+            <li>{"Lottiefiles"}</li>
+          </ul>
+        }
+      />
     </main>
   );
 }
